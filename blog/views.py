ed6c8c501e8c242b3761a351a3b9d1427486eb1c
ryohei <omita777@yahoo.co.jp>
2020-06-15 12:23:06 +0900

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+import os
 
 # Create your views here.
+def post_list(request):
+    #return render(request, os.getcwd() + '\\blog\\templetes\\blog\\post_list.html', {})
+    return render(request, 'blog/post_list.html', {})
